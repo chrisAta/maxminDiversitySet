9a86e0cf4982f5cdff089a38694637ef254c0dc5
Christian <c.atallah2@newcastle.ac.uk>
2018-07-25 16:10:14 +0100

    refactoring

--- data_prep.py
+++ data_prep.py
@@ -28,37 +28,3 @@ def get_matrix_min(dist):
     min_arr = np.where(dist == np.nanmin(dist))
 
     return min_arr[0]
-
-def greedy_min_max_alg(dist, headings, set, k):
-
-        while len(set) < k:
-
-            min_val = 10
-            min_ind = 0
-
-            for i in range(0, len(dist)):
-
-                if i in set:
-                    continue
-
-                rand = np.random.randint(0,10000)
-
-                #COMMENT THIS OUT FOR DETERMINISTIC
-                # if rand < 3:
-                #
-                #     print 'RANDOM ADDITION: ' + str(rand)
-                #     min_ind = i
-                #     break
-
-                max_val = np.nanmax(dist[i])
-
-                if max_val < min_val:
-
-                    min_val = max_val
-                    min_ind = i
-
-            print "ADDED %s" % (headings[min_ind])
-
-            set += [min_ind]
-
-        return set
---
+++ max_min_diversity.py
@@ -0,0 +1,71 @@
+import numpy as np
+import data_prep
+
+def greedy_min_max_alg(dist, headings, set, k):
+
+        while len(set) < k:
+
+            min_val = 10
+            min_ind = 0
+
+            for i in range(0, len(dist)):
+
+                if i in set:
+                    continue
+
+                rand = np.random.randint(0,10000)
+
+                #COMMENT THIS OUT FOR DETERMINISTIC
+                # if rand < 3:
+                #
+                #     print 'RANDOM ADDITION: ' + str(rand)
+                #     min_ind = i
+                #     break
+
+                max_val = np.nanmax(dist[i])
+
+                if max_val < min_val:
+
+                    min_val = max_val
+                    min_ind = i
+
+            print "ADDED %s" % (headings[min_ind])
+
+            set += [min_ind]
+
+        return set
+
+
+
+def compute_diverse_set(dist_file, heading_file, k):
+
+    set = []
+
+    dist = data_prep.initialise_matrix(dist_file)
+
+    headings = data_prep.initialise_headings(heading_file)
+    #
+    # dist = data_prep.initialise_matrix('./temp_ssn_identities.npy')
+    #
+    # headings = data_prep.initialise_headings('temp_ssn_headings.json')
+    #
+    # dist = data_prep.initialise_matrix('./temp_csn_identities.npy')
+    #
+    # headings = data_prep.initialise_headings('temp_csn_headings.json')
+
+    min = data_prep.get_matrix_min(dist)
+
+    print "MIN: %s-%s" % (headings[min[0]], headings[min[1]])
+
+    set += [min[0], min[1]]
+
+    # set += [np.random.randint(0,241)]
+    #
+    # set += [np.random.randint(0,241)]
+
+
+    set = greedy_min_max_alg(dist, headings, set, k)
+
+    set = sorted([headings[x] for x in set])
+
+    print set
